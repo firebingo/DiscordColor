851f110269142243044e5ca77241b0eca9b50b6d
firebingo <matthew.t.kides@gmail.com>
2019-10-25 12:00:21 -0700

    Initial commit. Was developed some time ago so its pretty much "done".

---
+++ DiscordColor/Helpers.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace DiscordColor
+{
+	public static class Helpers
+	{
+		public static bool IsHex(string i)
+		{
+			if (i.Length == 7)
+				return Regex.IsMatch(i, "#[a-fA-F0-9]{6}");
+			else if (i.Length == 4)
+				return Regex.IsMatch(i, "#[a-fA-F0-9]{3}");
+			else
+				return false;
+		}
+
+		//https://www.w3.org/TR/2008/REC-WCAG20-20081211/#contrast-ratiodef
+		public static double GetContrast(string foreground, string background)
+		{
+			if (!IsHex(foreground) || !IsHex(background))
+				return -1.0;
+			Color c1 = ColorTranslator.FromHtml(foreground);
+			Color c2 = ColorTranslator.FromHtml(background);
+			var l1 = GetLuminance(c1);
+			var l2 = GetLuminance(c2);
+			if(l1 > l2)
+				return (l1 + 0.05) / (l2 + 0.05);
+			else
+				return (l2 + 0.05) / (l1 + 0.05);
+		}
+
+		//https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
+		private static double GetLuminance(Color color)
+		{
+			double[] a = new double[3] { color.R, color.G, color.B };
+			for(var i = 0; i < 3; ++i)
+			{
+				var v = a[i] / 255;
+				a[i] = v  < 0.03928 ? v / 12.92 : Math.Pow((v + 0.055) / 1.055, 2.4);
+			}
+			return 0.2126 * a[0] + 0.7152 * a[1] + 0.0722 * a[2];
+		}
+
+		public static string LightenColor(string hex)
+		{
+			if (!IsHex(hex))
+				return hex;
+			Color c = ColorTranslator.FromHtml(hex);
+			c = Color.FromArgb(Math.Min(255, c.R + 1), Math.Min(255, c.G + 1), Math.Min(255, c.B + 1));
+			return ColorTranslator.ToHtml(c);
+		}
+
+		public static string DarkenColor(string hex)
+		{
+			if (!IsHex(hex))
+				return hex;
+			Color c = ColorTranslator.FromHtml(hex);
+			c = Color.FromArgb(Math.Max(0, c.R - 1), Math.Max(0, c.G - 1), Math.Max(0, c.B - 1));
+			return ColorTranslator.ToHtml(c);
+		}
+
+		public static string SaturateColor(string hex)
+		{
+			if (!IsHex(hex))
+				return hex;
+			Color c = ColorTranslator.FromHtml(hex);
+			var hsv = HSVColor.ColorToHSV(c);
+			var newSat = Math.Min(1.0, hsv.s + 0.05);
+			c = HSVColor.ColorFromHSV(hsv.h, newSat, hsv.v);
+			return ColorTranslator.ToHtml(c);
+		}
+
+		public static string DullColor(string hex)
+		{
+			if (!IsHex(hex))
+				return hex;
+			Color c = ColorTranslator.FromHtml(hex);
+			var hsv = HSVColor.ColorToHSV(c);
+			var newSat = Math.Max(0, hsv.s - 0.05);
+			c = HSVColor.ColorFromHSV(hsv.h, newSat, hsv.v);
+			return ColorTranslator.ToHtml(c);
+		}
+	}
+
+	public struct HSVColor
+	{
+		public double h { get; }
+		public double s { get; }
+		public double v { get; }
+
+		public HSVColor(double h, double s, double v)
+		{
+			this.h = h;
+			this.s = s;
+			this.v = v;
+		}
+
+		//https://stackoverflow.com/questions/359612/how-to-change-rgb-color-to-hsv
+		public static HSVColor ColorToHSV(Color color)
+		{
+			int max = Math.Max(color.R, Math.Max(color.G, color.B));
+			int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+			double hue = color.GetHue();
+			double saturation = (max == 0) ? 0 : 1.0 - (1.0 * min / max);
+			double value = max / 255.0;
+			return new HSVColor(hue, saturation, value);
+		}
+
+		public static Color ColorFromHSV(double hue, double saturation, double value)
+		{
+			int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+			double f = hue / 60 - Math.Floor(hue / 60);
+
+			value = value * 255;
+			int v = Convert.ToInt32(value);
+			int p = Convert.ToInt32(value * (1 - saturation));
+			int q = Convert.ToInt32(value * (1 - f * saturation));
+			int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+			if (hi == 0)
+				return Color.FromArgb(255, v, t, p);
+			else if (hi == 1)
+				return Color.FromArgb(255, q, v, p);
+			else if (hi == 2)
+				return Color.FromArgb(255, p, v, t);
+			else if (hi == 3)
+				return Color.FromArgb(255, p, q, v);
+			else if (hi == 4)
+				return Color.FromArgb(255, t, p, v);
+			else
+				return Color.FromArgb(255, v, p, q);
+		}
+	}
+}
---
+++ DiscordColor/MainWindow.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiscordColor
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public GridData GData = new GridData();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			MainGrid.DataContext = GData;
+		}
+
+		private void LightenColor(object sender, RoutedEventArgs e)
+		{
+			if (sender is Button button)
+			{
+				if (button.Name == "LightenOne" && Helpers.IsHex(GData.Color1))
+					GData.Color1 = Helpers.LightenColor(GData.Color1);
+				else if(button.Name == "LightenTwo" && Helpers.IsHex(GData.Color2))
+					GData.Color2 = Helpers.LightenColor(GData.Color2);
+				else if (button.Name == "LightenThree" && Helpers.IsHex(GData.Color2))
+					GData.Color3 = Helpers.LightenColor(GData.Color3);
+			}
+		}
+
+		private void DarkenColor(object sender, RoutedEventArgs e)
+		{
+			if (sender is Button button)
+			{
+				if (button.Name == "DarkenOne" && Helpers.IsHex(GData.Color1))
+					GData.Color1 = Helpers.DarkenColor(GData.Color1);
+				else if (button.Name == "DarkenTwo" && Helpers.IsHex(GData.Color2))
+					GData.Color2 = Helpers.DarkenColor(GData.Color2);
+				else if (button.Name == "DarkenThree" && Helpers.IsHex(GData.Color2))
+					GData.Color3 = Helpers.DarkenColor(GData.Color3);
+			}
+		}
+
+		private void SaturateClick(object sender, RoutedEventArgs e)
+		{
+			if (sender is Button button)
+			{
+				if (button.Name == "VibrateOne" && Helpers.IsHex(GData.Color1))
+					GData.Color1 = Helpers.SaturateColor(GData.Color1);
+				else if (button.Name == "VibrateTwo" && Helpers.IsHex(GData.Color2))
+					GData.Color2 = Helpers.SaturateColor(GData.Color2);
+				else if (button.Name == "VibrateThree" && Helpers.IsHex(GData.Color2))
+					GData.Color3 = Helpers.SaturateColor(GData.Color3);
+			}
+		}
+
+		private void DullClick(object sender, RoutedEventArgs e)
+		{
+			if (sender is Button button)
+			{
+				if (button.Name == "DullOne" && Helpers.IsHex(GData.Color1))
+					GData.Color1 = Helpers.DullColor(GData.Color1);
+				else if (button.Name == "DullTwo" && Helpers.IsHex(GData.Color2))
+					GData.Color2 = Helpers.DullColor(GData.Color2);
+				else if (button.Name == "DullThree" && Helpers.IsHex(GData.Color2))
+					GData.Color3 = Helpers.SaturateColor(GData.Color3);
+			}
+		}
+	}
+
+	public class GridData : INotifyPropertyChanged
+	{
+		//private string _loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+		private string _loremIpsum = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789";
+		public string LoremIpsum
+		{
+			get
+			{
+				return _loremIpsum;
+			}
+		}
+
+		public string DarkBackground
+		{
+			get
+			{
+				return "#36393e";
+			}
+		}
+
+		public string LightBackground
+		{
+			get
+			{
+				return "#FFFFFF";
+			}
+		}
+
+		private string _color1 = "#000000";
+		public string Color1
+		{
+			get
+			{
+				return _color1;
+			}
+			set
+			{
+				_color1 = value;
+				NotifyPropertyChanged("Color1");
+				NotifyPropertyChanged("OneLightContrast");
+				NotifyPropertyChanged("OneDarkContrast");
+			}
+		}
+
+		private string _color2 = "#000000";
+		public string Color2
+		{
+			get
+			{
+				return _color2;
+			}
+			set
+			{
+				_color2 = value;
+				NotifyPropertyChanged("Color2");
+				NotifyPropertyChanged("TwoLightContrast");
+				NotifyPropertyChanged("TwoDarkContrast");
+			}
+		}
+
+		private string _color3 = "#000000";
+		public string Color3
+		{
+			get
+			{
+				return _color3;
+			}
+			set
+			{
+				_color3 = value;
+				NotifyPropertyChanged("Color3");
+				NotifyPropertyChanged("ThreeLightContrast");
+				NotifyPropertyChanged("ThreeDarkContrast");
+			}
+		}
+
+		public double OneLightContrast
+		{
+			get
+			{
+				return Helpers.GetContrast(LightBackground, Color1);
+			}
+		}
+
+		public double TwoLightContrast
+		{
+			get
+			{
+				return Helpers.GetContrast(LightBackground, Color2);
+			}
+		}
+
+		public double ThreeLightContrast
+		{
+			get
+			{
+				return Helpers.GetContrast(LightBackground, Color3);
+			}
+		}
+
+		public double OneDarkContrast
+		{
+			get
+			{
+				return Helpers.GetContrast(DarkBackground, Color1);
+			}
+		}
+
+		public double TwoDarkContrast
+		{
+			get
+			{
+				return Helpers.GetContrast(DarkBackground, Color2);
+			}
+		}
+
+		public double ThreeDarkContrast
+		{
+			get
+			{
+				return Helpers.GetContrast(DarkBackground, Color3);
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(string info)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+		}
+	}
+}
